e86d8c03fff903fcfe58baeddd0b570ec9780b0c
Artem <artem_leti@mail.ru>
2019-11-19 18:07:14 +0300

    Add mosaik images grid Add hover block 'go to original'

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const API_KEY = 'aCYx8eJP8FQAqLZHjjxu1PPyFz0kNMqO';
 const API_GIF_ID = '3o7WIydEMfY4J8q8Ew';
 const LIMIT_GIF = 10;
 
-const API_SEARCH = 'http://api.giphy.com/v1/gifs/search';
+const API_SEARCH = 'https://api.giphy.com/v1/gifs/search';
 const API_REQUEST_SEARCH_GIF = `${API_SEARCH}?api_key=${API_KEY}&`;
 const getSearchGif	= (query, offset = 0, limit = LIMIT_GIF) =>
 	fetch(`${API_REQUEST_SEARCH_GIF}${buildQueryParams(
@@ -20,7 +20,7 @@ const getSearchGif	= (query, offset = 0, limit = LIMIT_GIF) =>
 		}
 	)}`).then(res => res.json());
 
-const API_TREND = 'http://api.giphy.com/v1/gifs/trending';
+const API_TREND = 'https://api.giphy.com/v1/gifs/trending';
 const API_REQUEST_TRENDS_GIF = `${API_TREND}?api_key=${API_KEY}&`;
 const getTrendGif	= (offset = 0, limit = LIMIT_GIF) =>
 	fetch(`${API_REQUEST_TRENDS_GIF}${buildQueryParams(
@@ -31,8 +31,8 @@ const getTrendGif	= (offset = 0, limit = LIMIT_GIF) =>
 	)}`).then(res => res.json());
 
 
-const API_URL = 'http://api.giphy.com/v1/gifs/random';
-const API_URL_GIF = 'http://api.giphy.com/v1/gifs';
+const API_URL = 'https://api.giphy.com/v1/gifs/random';
+const API_URL_GIF = 'https://api.giphy.com/v1/gifs';
 
 const API_REQUEST = `${API_URL}?api_key=${API_KEY}&`;
 const API_REQUEST_ID_GIF = `${API_URL_GIF}/${API_GIF_ID}?api_key=${API_KEY}&`;
