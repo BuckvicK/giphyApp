6b858e16e56222adb08054b46276417f223cc7bb
Artem <artem_leti@mail.ru>
2019-11-30 15:37:38 +0300

    Add log request

--- api/routers/apiRouters.js
+++ api/routers/apiRouters.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const middleware = require('./middleware');
 
 router.use(function timeLog(req, res, next) {
-	console.log('Time: ', Date.now());
+	const ts = Date.now();
+
+	const date_ob = new Date(ts);
+	const date = date_ob.getDate();
+	const month = date_ob.getMonth() + 1;
+	const year = date_ob.getFullYear();
+	const hours = date_ob.getHours();
+	const minutes = date_ob.getMinutes();
+
+	console.log(`[${date}.${month}.${year} ${hours}:${minutes}] `, req.url, ' from ', req.headers.origin);
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET');
 	next();
