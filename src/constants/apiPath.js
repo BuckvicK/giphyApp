6f13aaa77fc17dc94e03f18c561e3fee2f88d743
Artem <artem_leti@mail.ru>
2019-12-03 00:41:20 +0300

    Redesign navigation panel

--- src/constants/apiPath.js
+++ src/constants/apiPath.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:3001/api/';
-// const API_BASE_URL = 'https://afternoon-crag-75675.herokuapp.com/api/';
+// const API_BASE_URL = 'http://localhost:3001/api/';
+const API_BASE_URL = 'https://afternoon-crag-75675.herokuapp.com/api/';
 
 const API_TREND = `${API_BASE_URL}trends`;
 const API_SEARCH = `${API_BASE_URL}search`;
